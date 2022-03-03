9acc53009dd905e4ed890f405fc86738e6d1ca43
Jakub Sobel <jakub.sobel@yougov.com>
2022-03-03 12:38:25 +0100

    Add Partytown

---
+++ src/zone-flags.ts
@@ -0,0 +1,3 @@
+// Uncomment below to disable zone pathes:
+// (window as any).__Zone_disable_IntersectionObserver = true; // disable patch IntersectionObserver
+// (window as any).__Zone_disable_MutationObserver = true; // disable patch MutationObserver
